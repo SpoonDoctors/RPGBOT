94d4a8e7172a2dd232430bc2da3595f93d45a1b7
SpoonDoctors <davidsc8@mail.uc.edu>
2016-10-08 16:29:38 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 var HTTPS = require('https');
+var scenario = require('./scenarioRunner.js');
 
 var botID = process.env.BOT_ID;
 
@@ -6,18 +7,10 @@ function respond() {
   var request = JSON.parse(this.req.chunks[0]),
       botRegexKya = /(.|)*(k|K)ya!~/;
   
-  var waifuPhrases = [ "It's not like I l-like you or anything...", "_-kun is so moe!", "Do you think I'm kawaii, _?",
-                      "B-B-baka!", "_-senpai is the best!", "But isn't that... lewd?", "Kemy-kun is sugoi, but not as sugoi as _-senpai!", "Noooo!",
-                     "Your waifu is trashfu!", "http://i.imgur.com/8JIV2U5.png", "https://pbs.twimg.com/media/CmIzPnkUoAQAG0f.png",
-                     "I could never hate you, _-kun!", "Do you really mean it, _-senpai?", "Rolls? Why do they call her Rolls?", 
-                     "He's not really a mummy, is he?", "Happy waifu, happy laifu!", 
-                     "http://i.imgur.com/N4fJ3.jpg",
-                     "I m-made you tendies _-kun. I hope you like them...", "To me, you have all the goodboy points in the world.",
-                     "Hitler-sama did nothing wrong."]
 
   if(request.text && botRegexKya.test(request.text)) {
     this.res.writeHead(200);
-    postMessage(getReturnString(waifuPhrases[getRandomInt(0,waifuPhrases.length)], request.name));
+    postMessage(scenario.testMulti());
     this.res.end();
   }
   else {
