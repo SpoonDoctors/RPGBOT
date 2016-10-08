6be15a264e1edc38310afd5e7cf5b47014748bc1
SpoonDoctors <davidsc8@mail.uc.edu>
2016-10-08 16:35:11 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ function respond() {
 
   if(request.text && botRegexKya.test(request.text)) {
     this.res.writeHead(200);
-    postMessage(scenario.testMulti());
+    postMessage(scenario.testMulti);
     this.res.end();
   }
   else {
