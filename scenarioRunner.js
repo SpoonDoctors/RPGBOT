dc7fc4eba074941536a234416c37c0ade9a5cd05
SpoonDoctors <davidsc8@mail.uc.edu>
2016-10-09 03:41:47 -0400

    Update scenarioRunner.js

--- scenarioRunner.js
+++ scenarioRunner.js
@@ -1,9 +1,14 @@
 function parseStory(scenarioText) {
-  var myRegexp = /~.*~/g;
-  match = myRegexp.exec(myString);
+  var myString = "something format_abc";
+  var myRegexp = /(?:^|\s)format_(.*?)(?:\s|$)/g;
+  var match = myRegexp.exec(myString);
   var matches = 0;
   while (match != null) {
     matches = matches + 1;
+    // matched text: match[0]
+    // match start: match.index
+    // capturing group n: match[n]
+    console.log(match[0])
     match = myRegexp.exec(myString);
   }
   return matches.toString();
