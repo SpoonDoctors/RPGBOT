7ac5322e59b1943da957645a939f3cd07173b1e7
SpoonDoctors <davidsc8@mail.uc.edu>
2016-10-09 12:03:02 -0400

    Update scenarioRunner.js

--- scenarioRunner.js
+++ scenarioRunner.js
@@ -26,6 +26,7 @@ function parseStory(scenarioText){
   var matchingString = null;
   while (match != null) {
     matchingString = match[0];
+    frameArray[matchingString.substring(matchingString.length-4,matchingString.length-1)] = matchingString.substring(1 , matchingString.length-4);
     match = myRegexp.exec(scenarioText);
     index = index + 1;
   }
