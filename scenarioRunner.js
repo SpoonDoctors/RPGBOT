e0efdfda4f2a9ac11bb768e87d8452257bfdbdaf
SpoonDoctors <davidsc8@mail.uc.edu>
2016-10-09 03:29:31 -0400

    Update scenarioRunner.js

--- scenarioRunner.js
+++ scenarioRunner.js
@@ -1,4 +1,11 @@
 function parseStory(scenarioText) {
-  return scenarioText;
+  var myRegexp = /~.*~/;
+  var match = myRegexp.exec(myString);
+  var matches = 0;
+  while (match != null) {
+    matches = matches + 1;
+    match = myRegexp.exec(myString);
+  }
+  return matches.toString();
 }
 module.exports.parseStory = parseStory;
